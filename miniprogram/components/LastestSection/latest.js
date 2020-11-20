062eec45a4258f415d83bb63c20c052c6f63fa45
Jay Shi <25982097@qq.com>
2020-11-20 23:51:58 +0800

    component news and latest

---
+++ miniprogram/components/LastestSection/latest.js
@@ -0,0 +1,3 @@
+"use strict";
+Component({});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibGF0ZXN0LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsibGF0ZXN0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxTQUFTLENBQUMsRUFFVCxDQUFDLENBQUEiLCJzb3VyY2VzQ29udGVudCI6WyJDb21wb25lbnQoe1xyXG4gIFxyXG59KSJdfQ==
--- miniprogram/pages/main/main.ts
+++ miniprogram/pages/main/main.ts
@@ -2,7 +2,21 @@ var app = getApp<IAppOption>()
 
 Page({
   onLoad(){
-    
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: res => {
+              app.globalData.userInfo = res.userInfo              
+              if (app.userInfoReadyCallback) {
+                app.userInfoReadyCallback(res)
+              }
+            },
+          })
+        }else{
+        }
+      },
+    })
   },
   onShow(){
     
