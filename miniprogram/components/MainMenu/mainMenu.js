3602042272eabad3438c6eb25f4a6a82ce9ef436
Jay Shi <25982097@qq.com>
2020-12-01 00:47:08 +0800

    Main Page

---
+++ miniprogram/components/MainMenu/mainMenu.js
@@ -0,0 +1,3 @@
+"use strict";
+Component({});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbk1lbnUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJtYWluTWVudS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsU0FBUyxDQUFDLEVBRVQsQ0FBQyxDQUFBIiwic291cmNlc0NvbnRlbnQiOlsiQ29tcG9uZW50KHtcclxuICBcclxufSkiXX0=
